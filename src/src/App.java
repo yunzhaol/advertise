4203bd876a974623391eab0c71a51c7d177b080c
yunzhaol <yunzhao.li@mail.utoronto.ca>
2025-05-19 22:52:27 -0400

    delete duplicate src

--- src/src/App.java
+++
@@ -1,7 +0,0 @@
-// === App.java ===
-public class App {
-    public static void main(String[] args) {
-        PlatformController controller = new PlatformController();
-        controller.run();
-    }
-}
